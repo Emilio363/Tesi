276e5d157aeb736f4c7a150cc0fce6f01db45c39
Emilio363 <emilio@groppi.org>
2026-04-01 10:00:24 +0200

    all ready for pygame

--- GameOfLife/game-of-life.py
+++ GameOfLife/game-of-life.py
@@ -42,19 +42,22 @@ class Ambient:
             count = 0
             for dir in direction:
                 newpos = tuple(np.sum([dir, pos], axis = 0))
+                if any(i<0 for i in newpos):
+                    continue
                 try:
                     count += self.oldmat[newpos]
                 except IndexError:
                     pass
-            print(count)
+            #print(count)
             self.matrix[pos] = count == 3 or (self.oldmat[pos] and count == 2)
 
     def __str__(self):
-        return self.matrix.__str__()
+        return self.matrix.__str__() + "\n"
 
 mat = Ambient(2,5)
-mat.set([(_,2) for _ in range(5)])
+mat.set([(_,2) for _ in range(1,4)])
 #mat.set(list(product([1, 2, 3], repeat=2)))
 print(mat)
-mat.epoc()
-print(mat)
+for i in range(10):
+    mat.epoc()
+    print(mat)
