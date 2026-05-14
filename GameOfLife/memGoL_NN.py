54547e816ae6af57cbbb09be7280b656c4453e52
Emilio363 <emilio@groppi.org>
2026-05-14 18:20:24 +0200

    refactoring training

--- GameOfLife/memGoL_NN.py
+++ GameOfLife/memGoL_NN.py
@@ -1,20 +1,28 @@
+print("start")
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import sklearn
+from pandas import read_csv
 from time import time
 import memGoL_models as mm
 from tqdm import tqdm
+import os
 
-import torch
+from sklearn.metrics import confusion_matrix
+print("sklenar import")
+
+from torch import device, load
+from torch import save as torch_save
+from torch.optim import Adam
+from torch.cuda import is_available
+
 import torch.nn as nn
 import torch.nn.functional as F
-
+print("torch import")
 # IPERPARAMETER
 num_epochs = 5
 learning_rate = 0.001
 bechSize = 64
-df = pd.read_csv("matrix_evolution_data.csv", dtype=float)
+df = read_csv("matrix_evolution_data.csv", dtype=float)
 df = df.to_numpy()
 
 # Dividi il database in training e test
@@ -22,17 +30,30 @@ df = df.to_numpy()
 train_dl, val_dl, test_dl = mm.dlCreator(data = df[:, :-3], label = df[:, -3], bechsize= bechSize)
 
 model = mm.DumbModel()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = device("cuda" if is_available() else "cpu")
 model = model.to(device)  # Move the model to the selected device
 
 criterion = nn.BCELoss() # loss function
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = Adam(model.parameters(), lr=learning_rate)
 
 
 train_loss_array = []
 train_acc_array = []
 val_loss_array = []
 val_acc_array = []
+conf_matrix = { "TP" : 0 , "FP" : 0 , "TN" : 0 , "FN" : 0}
+
+
+
+model_path = "DumbModel" + ".pth"
+train = True
+if(model_path in os.listdir()):
+    print("MODEL FOUND - ATTEMPTING TO LOAD")
+    model.load_state_dict(load(model_path))
+    print("MODEL LOADED, SKIPPING TRAINING")
+    train = False
+
+
 
 for epoch in range(num_epochs):
     train_running_loss = 0.0
@@ -80,9 +101,14 @@ for epoch in range(num_epochs):
 
         ## forward + backprop + loss
         evalLabels = model(grids)
-        evalLabels = evalLabels.flatten()
         loss = criterion(evalLabels, labels)
-
+        labels = labels.data.cpu().numpy().ravel()
+        evalLabels = evalLabels.data.cpu().numpy().ravel()
+        print([evalLabels>0.5])
+        print(labels)
+        print(f"{type(evalLabels)}  {type(labels)}  {evalLabels.shape}  {labels.shape}")
+        cm = confusion_matrix(evalLabels, labels)
+        print(cm)
         val_running_loss += loss.item()
         val_acc += mm.BCEAccuracy(evalLabels, labels)
 
@@ -91,7 +117,10 @@ for epoch in range(num_epochs):
     val_acc_array.append(val_acc / i)
     val_loss_array.append(val_running_loss/i)
     print(f"Epoch: {epoch+1} | Train: {train_running_loss / i:.4f} | Val: {val_running_loss / len(val_dl):.4f} | Time: {time()-start:.2f}")
-
+    state = model.state_dict()
+    torch_save(state, "LINEAR_MNIST.pth") #save best model yet
+else:
+    print("MODEL LOADED, SKIPPING TRAINING")
 
 plt.figure()
 plt.plot(train_loss_array)
--- GameOfLife/memGoL_models.py
+++ GameOfLife/memGoL_models.py
@@ -1,73 +1,119 @@
 import sklearn
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from time import time
+from tqdm import tqdm
+from numpy import ndarray
+
+from torch import tensor, float32, sigmoid, device
+from torch.utils.data import TensorDataset, DataLoader
+from torch.nn import Module, Linear, Conv1d
+from torch.nn.functional import relu
+from torch.optim import Adam
 
 def BCEAccuracy(logit, target):
-    corrects = ((logit.data > 0.5) == target.data).sum()
+    corrects = ((logit.data > 0.5) == target.data).sum() / len(logit.data)
     return corrects.item()
 
-def dlCreator(data, label, bechsize):
+def dlCreator(data : ndarray, label : ndarray, bechsize : int) -> tuple[DataLoader, DataLoader, DataLoader] :
 
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(data, label, test_size = 0.2, train_size = 0.8)
     x_train, x_val, y_train, y_val = sklearn.model_selection.train_test_split(x_train, y_train, test_size = 0.2, train_size = 0.8)
 
 
-    X_train_tensor = torch.tensor(x_train, dtype=torch.float32)
-    y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
+    X_train_tensor = tensor(x_train, dtype=float32)
+    y_train_tensor = tensor(y_train, dtype=float32)
 
-    X_val_tensor = torch.tensor(x_val, dtype=torch.float32)
-    y_val_tensor = torch.tensor(y_val, dtype=torch.float32)
+    X_val_tensor = tensor(x_val, dtype=float32)
+    y_val_tensor = tensor(y_val, dtype=float32)
 
-    X_test_tensor = torch.tensor(x_test, dtype=torch.float32)
-    y_test_tensor = torch.tensor(y_test, dtype=torch.float32)
+    X_test_tensor = tensor(x_test, dtype=float32)
+    y_test_tensor = tensor(y_test, dtype=float32)
 
     X_train_tensor = X_train_tensor.unsqueeze(1)
     X_val_tensor = X_val_tensor.unsqueeze(1)
     X_test_tensor = X_test_tensor.unsqueeze(1)
 
-    train_dataset = torch.utils.data.TensorDataset(X_train_tensor, y_train_tensor)
-    val_dataset = torch.utils.data.TensorDataset(X_val_tensor, y_val_tensor)
-    test_dataset = torch.utils.data.TensorDataset(X_test_tensor, y_test_tensor)
+    train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+    val_dataset = TensorDataset(X_val_tensor, y_val_tensor)
+    test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
 
-    train_dl = torch.utils.data.DataLoader(train_dataset, batch_size=bechsize, shuffle=True)
-    val_dl = torch.utils.data.DataLoader(val_dataset, batch_size=bechsize, shuffle=True)
-    test_dl = torch.utils.data.DataLoader(test_dataset, batch_size=bechsize, shuffle=False)
+    train_dl = DataLoader(train_dataset, batch_size=bechsize, shuffle=True)
+    val_dl = DataLoader(val_dataset, batch_size=bechsize, shuffle=True)
+    test_dl = DataLoader(test_dataset, batch_size=bechsize, shuffle=False)
 
     return train_dl, val_dl, test_dl
 
 # stupid, dumb dense slow clueless average mid smart clever bright intelligent gifted brilliant genius
 
-class StupidModel(nn.Module):
+class StupidModel(Module):
     def __init__(self):
         super(StupidModel, self).__init__()
-        self.l0 = nn.Linear(5*3,3*3)
-        self.l1 = nn.Linear(3*3, 1)
+        self.l0 = Linear(5*3,3*3)
+        self.l1 = Linear(3*3, 1)
 
     def forward(self, x):
         x = self.l0(x)
-        x = F.relu(x)
+        x = relu(x)
         x = self.l1(x)
-        x = F.sigmoid(x)
+        x = sigmoid(x)
         return x.flatten()
 
-class DumbModel(nn.Module):
-    def __init__(self):
+class DumbModel(Module):
+    def __init__(self, loss, optimizer = Adam):
         super(DumbModel, self).__init__()
-        self.l0 = nn.Conv1d(in_channels = 1, out_channels=24, kernel_size=3 )
-        self.l1 = nn.Linear(24*13,6*13)
-        self.l2 = nn.Linear(6*13, 3*3)
-        self.l3 = nn.Linear(3*3, 1)
+        self.loss = loss
+        self.optimizer = optimizer
+        self.l0 = Conv1d(in_channels = 1, out_channels=24, kernel_size=3 )
+        self.l1 = Linear(24*13,6*13)
+        self.l2 = Linear(6*13, 3*3)
+        self.l3 = Linear(3*3, 1)
 
     def forward(self, x):
         x = self.l0(x)
-        x = F.relu(x)
+        x = relu(x)
         x = x.flatten(1)
         x = self.l1(x)
-        x = F.relu(x)
+        x = relu(x)
         x = self.l2(x)
-        x = F.relu(x)
+        x = relu(x)
         x = self.l3(x)
-        x = torch.sigmoid(x) 
+        x = sigmoid(x) 
         return x.flatten()
     
+    def training(self, train_dl : DataLoader, max_epochs : int, loss = None, optimizer = None) -> Module:
+        loss = loss if loss != None else self.loss
+        optimizer = optimizer if optimizer != None else self.optimizer
+        train_acc_array = []
+        train_loss_array = []
+        for epoch in range(max_epochs):
+            train_running_loss = 0.0
+            train_acc = 0.0
+            sample_count = 0
+
+            self.train()
+            start = time()
+
+            for grids, labels in tqdm(train_dl, desc=f"Epoch {epoch+1}/{max_epochs}"):
+                grids = grids.to(device)
+                labels = labels.to(device)
+
+                evalLabels = self(grids)
+                loss = loss(evalLabels, labels)
+
+                optimizer.zero_grad()
+
+                loss.backward()
+                optimizer.step()
+
+                train_running_loss += loss.item()
+                train_acc += BCEAccuracy(evalLabels, labels)
+            
+
+            i = len(train_dl)
+            train_acc_array.append(train_acc / i)
+            train_loss_array.append(train_running_loss/i)
+            self.eval()
+        
+            print(f"Epoch: {epoch+1} | Train: {train_running_loss / i:.4f} | Time: {time()-start:.2f}")
+            state = self.state_dict()
+    
+        return state, train_loss_array, train_acc_array
